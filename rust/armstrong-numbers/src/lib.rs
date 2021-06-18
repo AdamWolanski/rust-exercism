77ab398833a76d8101d9e090c341dbfd54ef96c0
Adam Wolanski <adam.wolanski@protonmail.com>
2021-06-18 02:03:50 +0200

    Add exercism tasks 11-16
    - armstrong-numbers
    - gigasecond
    - high-scores
    - matching-brackets
    - reverse-string

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_len = num.to_string().len();
+    let num_str = num.to_string();
+
+    let out = num_str.chars().fold(0, |acc, f| {
+        acc + f.to_digit(10).unwrap().pow(num_len as u32)
+    });
+
+    return out == num
 }
